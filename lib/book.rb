cfdaacfff447ee99c19f3a2f52a1c37eb69c1694
Mateus Alves <mateusmpa@gmail.com>
2016-04-21 18:42:34 -0300

    Instance Attributes

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,9 @@
 class Book
   def initialize(author, isbn = '1', number_of_pages)
+    @author = author
+    @isbn = isbn
+    @number_of_pages = number_of_pages
+
     puts "Author #{author}, ISBN: #{isbn}, Pág: #{number_of_pages}"
   end
 end
