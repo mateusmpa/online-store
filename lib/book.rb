913ff45885fd3465a0fbd2976629bea264ce64bf
Mateus Alves <mateusmpa@gmail.com>
2016-04-21 19:20:26 -0300

    Add attribute price with attr_accessor

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,11 @@
 class Book
-  def initialize(author, isbn = '1', number_of_pages)
+  attr_accessor :price
+  
+  def initialize(author, isbn = '1', number_of_pages, price)
     @author = author
     @isbn = isbn
     @number_of_pages = number_of_pages
+    @price = price
   end
 
   def to_s
